8cd117e0459620efb37c2c6bb8593ab597b27c05
AngryAsho <angryasho@gmail.com>
2019-04-01 17:59:53 +0300

    Added skeleton.

---
+++ SomeSolution/SomeStartup/SomeProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeStartup
+{
+    class SomeProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some code");
+        }
+    }
+}
